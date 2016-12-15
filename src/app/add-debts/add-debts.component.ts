4a789ebf2da425650cae15425281944a318831b4
Syed Aman <sna274@nyu.edu>
2016-12-15 13:14:48 -0500

    migrate from CSS to SCSS

--- src/app/add-debts/add-debts.component.ts
+++ src/app/add-debts/add-debts.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-debts',
   templateUrl: './add-debts.component.html',
-  styleUrls: ['./add-debts.component.css']
+  styleUrls: ['./add-debts.component.scss']
 })
 export class AddDebtsComponent implements OnInit {
 
